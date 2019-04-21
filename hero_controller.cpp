968056fb8e87f9dc70fa5b0eb3d7742366724066
Patrick Connelly <patricksparbanieconnelly@gmail.com>
2019-04-21 04:30:10 -0400

    allow simultaneous vertical/horizontal movement in HeroController

--- hero_controller.cpp
+++ hero_controller.cpp
@@ -19,43 +19,26 @@ void HeroController::update(float dt) {
   if (gm) {
     if (gm->playerInput.upPressed) {
       oldVelocity.setY(-moveAmount);
-      //RealPoint *newPos = this->_sprite->getPos();
-      //newPos->setY(newPos->Y() - moveAmount);
-      //this->_sprite->setPos(*newPos);
 
     } else if (gm->playerInput.downPressed) {
       oldVelocity.setY(moveAmount);
-      //RealPoint *newPos = this->_sprite->getPos();
-      //newPos->setY(newPos->Y() + moveAmount);
-      //this->_sprite->setPos(*newPos);
 
-    } else if (gm->playerInput.leftPressed) {
+    } else {
+      oldVelocity.setY(0.0);
+    }
+
+    if (gm->playerInput.leftPressed) {
       oldVelocity.setX(-moveAmount);
-      //RealPoint *newPos = this->_sprite->getPos();
-      //newPos->setX(newPos->X() - moveAmount);
-      //this->_sprite->setPos(*newPos);
 
     } else if (gm->playerInput.rightPressed) {
       oldVelocity.setX(moveAmount);
-      //RealPoint *newPos = this->_sprite->getPos();
-      //newPos->setX(newPos->X() + moveAmount);
-      //this->_sprite->setPos(*newPos);
 
     } else {
       oldVelocity.setX(0.0);
-      oldVelocity.setY(0.0);
-
     }
-
-
   }
 
   this->_sprite->setVelocity(oldVelocity);
   this->_sprite->move(dt);
 
-  // can we call parent update?
-  // even if so, is it better to leave
-  // controllers enough flexibility
-  // to update at their will, or
-  // would responding to velocity be good enough?
 }
