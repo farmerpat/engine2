2189a56b211cad552db2599ab8b61ff07aa7be2d
Patrick Connelly <patricksparbanieconnelly@gmail.com>
2019-05-10 11:12:16 -0400

    init collision handling in MatrixOfSprites

--- src/matrix_of_sprites.cpp
+++ src/matrix_of_sprites.cpp
@@ -13,6 +13,8 @@ MatrixOfSprites::MatrixOfSprites
   this->_texture = Util::loadTexture(img, r);
   this->setXScale(xScale);
   this->setYScale(yScale);
+  this->_isAggregate = true;
+  this->setLayer(1);
 
   for (int i=0; i<this->_numRows; i++) {
     this->_matrix.push_back(std::vector<int>(this->_numCols, 1));
@@ -52,7 +54,7 @@ void MatrixOfSprites::render(SDL_Renderer *renderer) {
 
     for (int j=0; j<this->_numCols; j++) {
       if (this->_matrix[i][j]) {
-        x = xBase + (j * (this->_height + this->_xPad));
+        x = xBase + (j * (this->_width + this->_xPad));
         texture_dest_rect.x = x;
         texture_dest_rect.y = y;
         texture_dest_rect.w = this->getWidth() * this->_xScale;
@@ -65,8 +67,77 @@ void MatrixOfSprites::render(SDL_Renderer *renderer) {
 }
 
 void MatrixOfSprites::collisionHandler(std::unique_ptr<Sprite> &other) {
+  SDL_Rect theirHitBox = other->getGlobalHitBox();
+  SDL_Rect myHitBox;
+  int xBase = (int)this->getPos()->X();
+  int yBase = (int)this->getPos()->Y();
+  int x;
+  int y;
+  int height = this->_height;
+  int width = this->_width;
+  int xPad = this->_xPad;
+  int yPad = this->_yPad;
+  int xScale = this->_xScale;
+  int yScale = this->_yScale;
+
+  for (int i=0; i<this->_numRows; i++) {
+    y = yBase + (i * (this->_width + this->_yPad));
+    myHitBox.y = y;
+
+    for (int j=0; j<this->_numCols; j++) {
+      if (this->_matrix[i][j]) {
+        x = xBase + (j * (width + xPad));
+        myHitBox.x = x;
+        myHitBox.w = width * xScale;
+        myHitBox.h = height * yScale;
+
+        if (SDL_HasIntersection(&myHitBox, &theirHitBox)) {
+          if (other->getTag() == "player_bullet") {
+            this->_matrix[i][j] = 0;
+            other->kill();
+
+          }
+        }
+      }
+    }
+  }
 }
 
 bool MatrixOfSprites::isCollidingWith(std::unique_ptr<Sprite> &other) {
+  // TODO: this could be more effecient. should be able to limit
+  // i/j max based on other's hitbox. similarly for collisionHandler
+  // furthermore, this is the same shape as collisionHandler. factor this out
+  SDL_Rect theirHitBox = other->getGlobalHitBox();
+  SDL_Rect myHitBox;
+  int xBase = (int)this->getPos()->X();
+  int yBase = (int)this->getPos()->Y();
+  int x;
+  int y;
+  int height = this->_height;
+  int width = this->_width;
+  int xPad = this->_xPad;
+  int yPad = this->_yPad;
+  int xScale = this->_xScale;
+  int yScale = this->_yScale;
+
+  for (int i=0; i<this->_numRows; i++) {
+    y = yBase + (i * (this->_width + this->_yPad));
+    myHitBox.y = y;
+
+    for (int j=0; j<this->_numCols; j++) {
+      if (this->_matrix[i][j]) {
+        x = xBase + (j * (width + xPad));
+        myHitBox.x = x;
+        myHitBox.w = width * xScale;
+        myHitBox.h = height * yScale;
+
+        if (SDL_HasIntersection(&myHitBox, &theirHitBox)) {
+          return true;
+
+        }
+      }
+    }
+  }
+
   return false;
 }
